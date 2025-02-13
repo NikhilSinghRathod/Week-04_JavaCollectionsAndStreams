56bf649999234e25e11a1a9742c5c067a0f5b6f8
NikhilSinghRathod <r7445580@gmail.com>
2025-02-14 01:58:57 +0530

    [Nikhil Singh Rathode] Add. Program of Queue Interface To Implement Circular Buffer Simulation

---
+++ src/main/java/com/tit/day02javacollections/queueinterface/circularbuffersimulation/CircularBufferSimulation.java
@@ -0,0 +1,29 @@
+package com.tit.day02javacollections.queueinterface.circularbuffersimulation;
+
+// Creating a class CircularBufferSimulation to demonstrate the circular buffer
+public class CircularBufferSimulation
+{
+    public static void main(String[] args) {
+        // Creating a circular buffer with a capacity of 3
+        CircularBuffer buffer = new CircularBuffer(4);
+
+        // Inserting elements into the buffer
+        buffer.insert(1);
+        buffer.insert(2);
+        buffer.insert(3);
+        buffer.insert(4);
+
+        System.out.println("Buffer after inserting 1, 2, 3, four: " + buffer);
+
+        // Inserting another element to overwrite the oldest element
+        buffer.insert(5);
+        System.out.println("Buffer after inserting 5: " + buffer);
+
+        // Inserting more elements
+        buffer.insert(6);
+        buffer.insert(7);
+        System.out.println("Buffer after inserting 6, 7: " + buffer);
+    }
+}
+
+
