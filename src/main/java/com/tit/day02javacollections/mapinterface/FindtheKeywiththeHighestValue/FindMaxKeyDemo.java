bcf546c408bec8285935fd8d438bad00759953e1
NikhilSinghRathod <r7445580@gmail.com>
2025-02-14 02:20:36 +0530

    [Nikhil Singh Rathode] Add. Program of Map Interface to Find the max Key

---
+++ src/main/java/com/tit/day02javacollections/mapinterface/FindtheKeywiththeHighestValue/FindMaxKeyDemo.java
@@ -0,0 +1,19 @@
+package com.tit.day02javacollections.mapinterface.FindtheKeywiththeHighestValue;
+
+import java.util.Map;
+
+// Creating a class FindMaxKeyDemo to demonstrate finding the key with the maximum value
+public class FindMaxKeyDemo {
+    public static void main(String[] args) {
+        // Creating a map with test data
+        Map<String, Integer> map = Map.of("A", 10, "B", 20, "C", 15);
+
+        // Creating an object of FindMaxKey
+        FindMaxKey findMaxKey = new FindMaxKey();
+
+        // Finding and printing the key with the maximum value
+        String maxKey = findMaxKey.findMaxKey(map);
+        System.out.println("The key with the highest value is: " + maxKey);
+    }
+}
+
