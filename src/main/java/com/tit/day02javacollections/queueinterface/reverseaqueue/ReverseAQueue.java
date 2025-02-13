cd060f0f0e57fef9b04df98fb3b5c08f5b3d5284
NikhilSinghRathod <r7445580@gmail.com>
2025-02-14 01:07:53 +0530

    [Nikhil Singh Rathode] Add. Program of Queue Interface To reverse a queue

---
+++ src/main/java/com/tit/day02javacollections/queueinterface/reverseaqueue/ReverseAQueue.java
@@ -0,0 +1,31 @@
+package com.tit.day02javacollections.queueinterface.reverseaqueue;
+
+import java.util.Queue;
+
+import java.util.*;
+
+// Creating a class ReverseAQueue to demonstrate reversing elements in a queue
+public class ReverseAQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the elements of the queue
+        System.out.print("Enter the elements of the queue (comma-separated): ");
+        String[] input = scanner.nextLine().split(",");
+        Queue<Integer> queue = new LinkedList<>();
+        for (String s : input) {
+            queue.add(Integer.parseInt(s.trim()));
+        }
+
+        // Creating an object of Reverse
+        Reverse reverse = new Reverse();
+
+        // Reversing the queue
+        queue = reverse.reverse(queue);
+
+        // Printing the reversed queue
+        System.out.println("Reversed queue: " + queue);
+    }
+}
+
+
