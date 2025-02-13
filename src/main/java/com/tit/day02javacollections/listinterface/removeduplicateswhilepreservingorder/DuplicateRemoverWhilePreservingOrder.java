caec03eccaaa39572e2a1dea32760c3ae302537c
NikhilSinghRathod <r7445580@gmail.com>
2025-02-13 23:52:46 +0530

    [Nikhil Singh Rathode] Add. Program04 of List interface Remove Duplicates While preserving the order

---
+++ src/main/java/com/tit/day02javacollections/listinterface/removeduplicateswhilepreservingorder/DuplicateRemoverWhilePreservingOrder.java
@@ -0,0 +1,23 @@
+package com.tit.day02javacollections.listinterface.removeduplicateswhilepreservingorder;
+
+import java.util.*;
+
+// Creating a class DuplicateRemoverWhilePreservingOrder to demonstrate removing duplicates while preserving order
+public class DuplicateRemoverWhilePreservingOrder
+{
+    public static void main(String[] args) {
+        // Creating an object of DuplicateRemover
+        DuplicateRemover remover = new DuplicateRemover();
+
+        // List of integers with duplicates
+        List<Integer> list = Arrays.asList(3, 1, 2, 2, 3, 4);
+
+        // Removing duplicates from the list
+        List<Integer> uniqueList = remover.removeDuplicates(list);
+
+        // Printing the original and unique lists
+        System.out.println("Original list: " + list);
+        System.out.println("List without duplicates: " + uniqueList);
+    }
+}
+
