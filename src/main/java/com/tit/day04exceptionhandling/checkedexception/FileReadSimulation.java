ec35eac7b09532501628722bfa021f2c5b84b13a
NikhilSinghRathod <r7445580@gmail.com>
2025-02-16 22:22:12 +0530

    [Nikhil Singh Rathode] Add. Program01 Of checked Exception

---
+++ src/main/java/com/tit/day04exceptionhandling/checkedexception/FileReadSimulation.java
@@ -0,0 +1,15 @@
+package com.tit.day04exceptionhandling.checkedexception;
+
+// Creating a class FileReadSimulation to demonstrate file reading operation
+public class FileReadSimulation
+{
+    public static void main(String[] args)
+    {
+        // Creating an object of FileHandler
+        FileHandler fileHandler = new FileHandler();
+        // File name
+        String fileName = "src/main/resources/data.txt";
+        // Reading the file
+        fileHandler.readFile(fileName);
+    }
+}
