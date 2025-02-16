4616eabf5c363d5d7ac78e64f70d088d3d87283b
NikhilSinghRathod <r7445580@gmail.com>
2025-02-16 23:40:57 +0530

    [Nikhil Singh Rathode] Add. Program06 of Throw Vs Throws

---
+++ src/main/java/com/tit/day04exceptionhandling/thorwvsthrows/InterestCalculator.java
@@ -0,0 +1,17 @@
+package com.tit.day04exceptionhandling.thorwvsthrows;
+
+
+// Creating a class InterestCalculator to handle interest calculations
+public class InterestCalculator {
+
+    // Method to calculate interest and propagate IllegalArgumentException if input is invalid
+    public double calculateInterest(double amount, double rate, int years) throws IllegalArgumentException
+    {
+        if (amount < 0 || rate < 0) {
+            throw new IllegalArgumentException("Amount and rate must be positive");
+        }
+        // Calculate interest
+        double interest = amount * rate * years / 100;
+        return interest;
+    }
+}
