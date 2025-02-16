09e0b49f87941d67406037c2455ff2db6a500b7f
NikhilSinghRathod <r7445580@gmail.com>
2025-02-16 22:34:17 +0530

    [Nikhil Singh Rathode] Add. Program02 Of UnChecked Exception

---
+++ src/main/java/com/tit/day04exceptionhandling/uncheckedexception/DivisionSimulation.java
@@ -0,0 +1,13 @@
+package com.tit.day04exceptionhandling.uncheckedexception;
+
+// Creating a class DivisionSimulation to demonstrate division operation
+public class DivisionSimulation
+{
+    public static void main(String[] args)
+    {
+        // Creating an object of DivisionHandler
+        DivisionHandler divisionHandler = new DivisionHandler();
+        // Performing the division
+        divisionHandler.divideNumbers();
+    }
+}
