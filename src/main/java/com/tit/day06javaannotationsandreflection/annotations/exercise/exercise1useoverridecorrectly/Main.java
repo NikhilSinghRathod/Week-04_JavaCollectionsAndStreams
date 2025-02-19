14599316b706a012262582dc9d7ee7032c31b850
NikhilSinghRathod <r7445580@gmail.com>
2025-02-19 23:56:30 +0530

    [Nikhil Singh Rathode] Add. Exercise 1: Use @OverRide Correctly

---
+++ src/main/java/com/tit/day06javaannotationsandreflection/annotations/exercise/exercise1useoverridecorrectly/Main.java
@@ -0,0 +1,11 @@
+package com.tit.day06javaannotationsandreflection.annotations.exercise.exercise1useoverridecorrectly;
+
+// Creating a main class to test the overridden method
+public class Main {
+    public static void main(String[] args) {
+        // Instantiating a Dog object
+        Dog dog = new Dog();
+        // Calling the makeSound() method
+        dog.makeSound(); // Output: Dog barks
+    }
+}
