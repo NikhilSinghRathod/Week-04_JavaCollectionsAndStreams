3be1446a581b820b4dc22913368f70e8a0470af5
NikhilSinghRathod <r7445580@gmail.com>
2025-02-20 11:43:04 +0530

    [Nikhil Singh Rathode] Add.Exercise 3 :Supress Unchecked Warnings

---
+++ src/main/java/com/tit/day06javaannotationsandreflection/annotations/exercise/exercise3suppressuncheckedwarnings/Main.java
@@ -0,0 +1,11 @@
+package com.tit.day06javaannotationsandreflection.annotations.exercise.exercise3suppressuncheckedwarnings;
+
+// Creating a main class to test the UncheckedWarningExample class
+public class Main {
+    public static void main(String[] args) {
+        // Instantiating an UncheckedWarningExample object
+        UncheckedWarningExample example = new UncheckedWarningExample();
+        // Calling the addToList() method
+        example.addToList();
+    }
+}
---
+++ src/main/java/com/tit/day06javaannotationsandreflection/annotations/exercise/exercise3suppressuncheckedwarnings/UncheckedWarningExample.java
@@ -0,0 +1,23 @@
+package com.tit.day06javaannotationsandreflection.annotations.exercise.exercise3suppressuncheckedwarnings;
+
+import java.util.ArrayList;
+
+// Creating a class UncheckedWarningExample to demonstrate suppressing unchecked warnings
+public class UncheckedWarningExample {
+
+    // Method to demonstrate suppressing unchecked warnings
+    @SuppressWarnings("unchecked")
+    public void addToList() {
+        // Creating an ArrayList without generics
+        ArrayList list = new ArrayList();
+
+        // Adding elements to the list
+        list.add("Hello");
+        list.add(123);
+
+        // Printing the elements of the list
+        for (Object element : list) {
+            System.out.println(element);
+        }
+    }
+}
